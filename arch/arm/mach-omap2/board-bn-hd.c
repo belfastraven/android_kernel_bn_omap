cff0e7ef71d828b6f237cfd971afbd679cb46542
Ziyan <jaraidaniel@gmail.com>
2016-07-14 00:10:56 +0200

    nookhd: main: Fix MUSB interface mode
    CONFIG_USB_MUSB_OTG isn't present in 3.4
    
    Change-Id: I1011051ce854148c71c1752dc74ef8001113a4a4
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- arch/arm/mach-omap2/board-bn-hd.c
+++ arch/arm/mach-omap2/board-bn-hd.c
@@ -104,11 +104,7 @@ static void __init omap_bn_init_early(void)
 
 static struct omap_musb_board_data musb_board_data = {
 	.interface_type		= MUSB_INTERFACE_UTMI,
-#ifdef CONFIG_USB_MUSB_OTG
 	.mode			= MUSB_OTG,
-#else
-	.mode			= MUSB_PERIPHERAL,
-#endif
 	.power			= 200,
 };
 
